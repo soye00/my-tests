7bfdfdfb4485923764bc51656722261dd9759a69
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-05-09 18:11:04 +0900

    no message

--- pwa/project_work/mapproject/src/App.jsx
+++ pwa/project_work/mapproject/src/App.jsx
@@ -1,19 +1,21 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import {Map, MapMarker, useKakaoLoader} from "react-kakao-maps-sdk";
-import {Card, message, Space} from "antd";
-import axios from "axios";
 import {fetchCities} from "../api/supadb.js";
 import {fetchAqi} from "../api/airapi.js";
 import AirTable from "./components/AirTable.jsx";
+import { Bar, Line, Pie } from '@ant-design/plots';
+import { XFilled } from '@ant-design/icons';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
   const [cities, setCities] = useState([]);
   const [aqiInfo, setAqiInfo] = useState({});
 
   useKakaoLoader({
       appkey : '79f216710859bc2ebef6080c725aa960',
+      libraries: ["clusterer", "services", "drawing"]
   })
 
   // supabase 에서 cities 데이터 가져오기
@@ -32,6 +34,19 @@ function App() {
     });
   }
 
+  const config = {
+    data : [
+      {x: '2025-05-01', y:10},
+      {x: '2025-05-02', y:20},
+      {x: '2025-05-03', y:50},
+      {x: '2025-05-04', y:40},
+      {x: '2025-05-05', y:20},
+    ],
+    xField: 'x',
+    yField: 'y',
+
+  }
+
   return (
     <>
       <h1>Hello</h1>
@@ -44,7 +59,10 @@ function App() {
               </MapMarker>
           ))}
       </Map>
-      <AirTable aqiInfo={aqiInfo}></AirTable>
+      <AirTable {...aqiInfo}></AirTable>
+      <Line data={config.data} xField={config.xField} yField={config.yField}></Line>
+      <Bar data={config.data} xField={config.xField} yField={config.yField}></Bar>
+      
 
 
     </>
--- pwa/project_work/mapproject/src/components/AirTable.jsx
+++ pwa/project_work/mapproject/src/components/AirTable.jsx
@@ -1,50 +1,79 @@
-import React from 'react';
-import {Card, Space, Table} from "antd";
+import React, { useEffect, useState } from "react";
+import { Card, Space, Table } from "antd";
+
 
 function AirTable(props) {
-    console.log('props', props);
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+  const [data, setData] = useState(props);
 
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
-        },
-        {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-    ];
+  useEffect(() => {
+    console.log("airTable useEffect");
+    setData(props);
+  }, [props]);
 
+  try {
+    const {
+      aqi,
+      city: { geo },
+      iaqi: { co, no2, o3, pm10, pm25, so2 },
+    } = props;
 
-    return (
-        <Space>
-            <Card hoverable>
-                <h1>대기질 정보</h1>
-                <Table dataSource={dataSource} columns={columns} />;
-            </Card>
-        </Space>
-    );
+    console.log(aqi);
+    console.log(geo);
+    console.log("일산화탄소", co);
+    console.log("이산화질소", no2);
+    console.log("오존", o3);
+    console.log("미세먼지", pm10);
+    console.log("초미세먼지", pm25);
+    console.log("아황산가스", so2);
+  } catch (e) {}
+
+  const dataSource = [
+    {
+      key: "1",
+      name: "0~50",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "AQI",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <Card hoverable style={{ margin: "2rem" }}>
+      <h1>대기질 정보 {data.aqi} </h1>
+
+        <h2>일산화탄소 : {data?.iaqi?.co?.v} </h2>
+        <h2>이산화질소 : {data?.iaqi?.no2?.v} </h2>
+        <h2>오존 : {data?.iaqi?.o3?.v} </h2>
+        <h2>미세먼지 : {data?.iaqi?.pm10?.v} </h2>
+        <h2>초미세먼지 : {data?.iaqi?.pm25?.v} </h2>
+        <h2>아황산가스 : {data?.iaqi?.so2?.v} </h2>
+      
+      <Table dataSource={dataSource} columns={columns} pagination={false} />;
+    </Card>
+  );
 }
 
 export default AirTable;
