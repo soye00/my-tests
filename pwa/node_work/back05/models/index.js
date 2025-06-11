daaeb34c9c0b7aeb69965d8f8c691a4eeb1f689b
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-06-11 17:55:14 +0900

    no message

--- pwa/node_work/back05/models/index.js
+++ pwa/node_work/back05/models/index.js
@@ -9,15 +9,19 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 
 console.log(config);
+// console.log(config.use_env_variable);
 
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    // DB 연결을 하겠다 -> orm 객체 생성을 insert,update,delete,select 함수로 호출할 준비 
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+// console.log(sequelize);
 //
 // fs
 //   .readdirSync(__dirname)
@@ -40,7 +44,25 @@ const db = {};
 //   }
 // });
 //
-// db.sequelize = sequelize;
+
+// seque 객체를 db 객체에 class 변수로 적용
+db.sequelize = sequelize;
+
+const User = require('./user');
+const Comment = require('./comment');
+
+db.User = User;
+db.Comment = Comment;
+
+User.initiate(sequelize);
+Comment.initiate(sequelize);
+
+User.associate(db);
+Comment.associate(db);
+
+
+
+
 // db.Sequelize = Sequelize;
 
 module.exports = db;
