7ce14d184b647b84e6c231fff18ee8e31555c65d
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-06-23 11:27:40 +0900

    no message

---
+++ pwa/cs_work/ConsoleApp_chap02/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 프로그램 시작점 
+            Console.WriteLine("안녕하세요");
+
+            int a = 10;
+            a += 20;
+            string c = a + "aaa";
+            bool b = 30<20;
+
+            //''->문자 하나일 때만 ex) 'a'
+            // 주석 ctrl + k +  c 
+            // 주석 해제 ctrl + k + u
+            // 정렬 ctrl k + d 
+
+
+            Console.WriteLine("a = " + a); //a = 30
+            Console.WriteLine("c = " + c); // c = 30aaa
+            Console.WriteLine(57 > 230); //false
+            Console.WriteLine(b); //false
+        }
+    }
+}
