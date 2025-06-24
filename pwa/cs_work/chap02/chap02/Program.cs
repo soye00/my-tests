c5d83197bd3ad43672963b3c9b92de173b90c3ff
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-06-24 18:42:27 +0900

    no message

---
+++ pwa/cs_work/chap02/chap02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap02
+{
+    internal class Program
+    {
+        // 프로그램 시작시 항상 존재하는 함수나 변수 => static 
+        // 반환값을 개발자가 적어줘야 함 void => return 값 생략. 
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕");
+
+            Console.WriteLine(32.GetType());
+            Console.WriteLine(129L.GetType());
+
+            Console.WriteLine(3.14f.GetType());
+            Console.WriteLine(3.14.GetType());
+
+            Console.WriteLine(true.GetType());
+            Console.WriteLine("안녕".GetType());
+            Console.WriteLine("A".GetType());
+            Console.WriteLine("안녕".Length);
+            Console.WriteLine("hello".ToUpper());
+            Console.WriteLine("hEllO".ToLower());
+            Console.WriteLine("안녕".Substring(1));
+
+            int a = 10;
+            double b = 3.12; // 문자 있는 숫자
+            string c = "hello";
+            bool d = false;
+            char e = 'A';
+
+            var f = "hello";
+            // var => 타입을 자동으로 추론 
+            Console.WriteLine(f.GetType()); //System.String
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ pwa/cs_work/chap02/win02/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text, "첫번째");
+            MessageBox.Show(textBox2.Text, "두번째");
+            MessageBox.Show(textBox1.Text + textBox2.Text, "더한값");
+
+
+            //int.Parse => string 을 int 로 전환
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            // MessageBox.Show => string 타입만 가능 .ToString 으로 바꿔서 넣어주기
+            MessageBox.Show((num1 + num2).ToString(),"더한값");
+            MessageBox.Show($"\"{num1 - num2}\"", "빼기값");
+            // 문자열 보간법 $"{}"
+            // \t -> 탭 만큼 이동
+            // \n -> 줄바꿈 
+
+
+        }
+    }
+}
