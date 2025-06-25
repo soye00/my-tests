1f08253a2ee28b08a8561d6887a9795ce6691a6b
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-06-25 19:36:38 +0900

    no message

---
+++ pwa/cs_work/chap03/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = 10;
+            MessageBox.Show($"number = {number}");
+            MessageBox.Show($"number = {number++}");
+            MessageBox.Show($"number = {number--}");
+            MessageBox.Show($"number = {number}");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = Console.ReadLine();
+            MessageBox.Show($"input = {input}");
+
+            // 
+            //MessageBox.Show(!true);
+            MessageBox.Show((!true).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            doA();
+            doA(100);
+            doA(100, 200);
+            doA("Hello");
+
+        }
+
+        public void doA()
+        {
+            MessageBox.Show("doA()함수");
+        }
+        public void doA(int aa)
+        {
+            MessageBox.Show($"doA()함수 aa = {aa}");
+        }
+        public void doA(int aa, int bb)
+        {
+            MessageBox.Show($"doA()함수 aa = {aa}, bb = {bb}");
+        }
+        public void doA(string cc)
+        {
+            MessageBox.Show($"doA()함수 cc = {cc}");
+        }
+
+    }
+}
---
+++ pwa/cs_work/chap03/conapp01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conapp01
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // int는 2^32 -> 4byte 까지 표현 가능
+            //int a = (int)1234564545321;
+
+            long b = 1234564545321;
+            int a = (int)b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            // 묵시적 형변환은 
+            // 데이터 손실이 일어날때는 선언해야함 
+            // long -> int : 선언해야함 
+            // int -> long : 선언하지 않아도 됨 
+
+            
+            
+            
+            
+            // 명시적 형변환
+            long e = 12345635131352L;
+            int f = (int)e;
+;
+
+
+
+
+
+        }
+    }
+
+    class AA
+    {
+        public void Test()
+        {
+            Console.WriteLine("AA 클래스의 Test");
+        }
+    }
+
+    class BB
+    {
+        public void Test()
+        {
+            Console.WriteLine("BB 클래스의 Test");
+        }
+    }
+
+
+
+
+
+
+
+}
